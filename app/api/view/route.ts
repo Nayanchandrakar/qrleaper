8fad9e079031b653cab02d93b20b1e5aef4478a4
Nayanchandrakar <chandrakarnayan96@gmail.com>
2024-12-17 12:24:27 +0530

    fix: /api/view route dynamic error

--- app/api/view/route.ts
+++ app/api/view/route.ts
@@ -1,5 +1,7 @@
 import { getFile } from "@/app/actions/file/getFile"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(req: Request) {
   try {
     const url = new URL(req.url)
