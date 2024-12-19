85d4f20bff753748777c71967939c61b81e0a324
Nayanchandrakar <chandrakarnayan96@gmail.com>
2024-12-19 10:36:37 +0530

    for

---
+++ app/(design)/design/(routes)/vcard/page.tsx
@@ -0,0 +1,11 @@
+import { VirtualCardForm } from "@/components/forms/pages/design/virtual-card/virtual-card-form"
+
+export const metadata = {
+  title: "Create Profile based QR Codes",
+}
+
+const VirtualCardPage = () => {
+  return <VirtualCardForm />
+}
+
+export default VirtualCardPage
