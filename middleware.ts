a33a80accaac7592ee3ab6c789d3e2cc924a305b
Nayanchandrakar <chandrakarnayan96@gmail.com>
2024-12-30 18:46:36 +0530

    feat: middleware matcher config update

--- middleware.ts
+++ middleware.ts
@@ -75,7 +75,9 @@ export default auth(async function middleware(req) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|[\\w-]+\\.\\w+).*)",
-    "/api/(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
 }
