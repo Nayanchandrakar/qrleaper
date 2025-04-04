9eefff9fec36c28723c194868b7b9dd69e7f5174
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-04-04 20:08:54 +0530

    chore: new migration schema

--- drizzle/0001_awesome_landau.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE "public"."gradient_type" AS ENUM('linear', 'radial');--> statement-breakpoint
-ALTER TABLE "qr_code_style" ADD COLUMN "colors" text[] NOT NULL;--> statement-breakpoint
-ALTER TABLE "qr_code_style" ADD COLUMN "rotation" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "qr_code_style" ADD COLUMN "gradient_type" "gradient_type" DEFAULT 'linear' NOT NULL;--> statement-breakpoint
-ALTER TABLE "qr_code_style" DROP COLUMN IF EXISTS "color";
