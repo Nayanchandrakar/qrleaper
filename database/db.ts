0ee2a893d55af1b1162b5c2158cf99d936037d21
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-04-16 15:54:08 +0530

    chore: use singleton pattern for database instance

--- database/db.ts
+++ database/db.ts
@@ -1,9 +1,24 @@
 import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { type NeonDatabase, drizzle } from "drizzle-orm/neon-serverless";
+
 import * as schema from "./schema";
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	max: 2000,
-});
-export const db = drizzle({ client: pool, schema: schema });
+type DatabaseType = NeonDatabase<typeof schema> & {
+	$client: Pool;
+};
+
+class DatabaseConnection {
+	private static instance: DatabaseType;
+	private constructor() {
+		const pool = new Pool({
+			connectionString: process.env.DATABASE_URL!,
+		});
+		DatabaseConnection.instance = drizzle(pool, { schema });
+	}
+	static getInstance(): DatabaseType {
+		if (!DatabaseConnection.instance) new DatabaseConnection();
+		return DatabaseConnection.instance;
+	}
+}
+
+export const db = DatabaseConnection.getInstance();
