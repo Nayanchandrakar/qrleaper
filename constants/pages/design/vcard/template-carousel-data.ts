98df163b76858d8bbbb89514c2b12eaccd4e3319
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-04-17 16:40:59 +0530

    added: new template images

--- constants/pages/design/vcard/template-carousel-data.ts
+++ constants/pages/design/vcard/template-carousel-data.ts
@@ -9,25 +9,25 @@ export const templateCarouselData = [
 	{
 		id: 434346346,
 		templateId: "A7B3C8D2",
-		image: "/vcard/template1.jpeg",
+		image: "/vcard/template1.png",
 		Component: CoreVcardPreview,
 	},
 	{
 		id: 456363464,
 		templateId: "ALSDFIO3",
-		image: "/vcard/template1.jpeg",
+		image: "/vcard/template2.png",
 		Component: BlueVcardPreview,
 	},
 	{
 		id: 346345345,
 		templateId: "DSFGHEEI",
-		image: "/vcard/template1.jpeg",
+		image: "/vcard/template3.png",
 		Component: OrangeGrayPreview,
 	},
 	{
 		id: 23452564,
 		templateId: "3ERWFDSD",
-		image: "/vcard/template1.jpeg",
+		image: "/vcard/template4.png",
 		Component: BrownPaperPreview,
 	},
 ];
