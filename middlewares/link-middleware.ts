90c4cb2b867b2c1e9b9164193bd3ff49668910ac
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-04-18 17:16:56 +0530

    fix: email and sms redirect issue

--- middlewares/link-middleware.ts
+++ middlewares/link-middleware.ts
@@ -15,7 +15,7 @@ import {
 	incrmentQrScanCountByQrCodeId,
 } from "@/app/actions/utils";
 import { sendEmail } from "@/lib/mail";
-import { redirectTo } from "@/middlewares/utils";
+import { handleRedirect, redirectTo } from "@/middlewares/utils";
 import { QrCodeLimitReached } from "@/templates/notifications/qr-limit-react-template";
 
 export const linkMiddleware = async (req: NextRequest) => {
@@ -87,10 +87,5 @@ export const linkMiddleware = async (req: NextRequest) => {
 		return redirectTo(nextUrl);
 	}
 
-	return new Response(null, {
-		status: 302,
-		headers: {
-			Location: qrCode?.endpoint!,
-		},
-	});
+	return handleRedirect(qrCode.type!, qrCode.endpoint!, nextUrl);
 };
--- middlewares/utils.ts
+++ middlewares/utils.ts
@@ -1,6 +1,24 @@
+import { qrType } from "@/types/db-types";
 import type { NextURL } from "next/dist/server/web/next-url";
 import { NextResponse } from "next/server";
 
 export const redirectTo = (nextURL: NextURL, path?: string) => {
 	return NextResponse.redirect(new URL(path ?? "/design", nextURL));
+};
+
+export const handleRedirect = (
+	type: qrType,
+	endpoint: string,
+	nextUrl: NextURL,
+) => {
+	if (["email", "sms"].includes(type)) {
+		return new Response(
+			`<meta http-equiv="refresh" content="0;url=${endpoint}">`,
+			{
+				headers: { "Content-Type": "text/html" },
+			},
+		);
+	} else {
+		return redirectTo(nextUrl, endpoint);
+	}
 };
